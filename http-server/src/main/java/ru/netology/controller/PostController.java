f292b891757c1aae7a7bfc0edad1b71eacbe4e76
dudez2025 <konstyushin@gmail.com>
2025-12-18 04:58:12 +0300

    feature/di-java commit

--- http-server/src/main/java/ru/netology/controller/PostController.java
+++ http-server/src/main/java/ru/netology/controller/PostController.java
@@ -1,17 +1,13 @@
 package ru.netology.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
 import java.util.List;
 
-@Controller
 public class PostController {
     private final PostService service;
     
-    @Autowired
     public PostController(PostService service) {
         this.service = service;
     }
